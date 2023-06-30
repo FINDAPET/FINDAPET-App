a91554c4b821d56d2754aa9c2e81534a8ec2a4c8
Артем Зюзин <artyomzuzin@MacBook-Pro-Artem.local>
2023-06-30 23:07:52 +0300

    pre-release 1.0

--- FINDAPET-App/Services/URLContructor/URLParts/Hosts.swift
+++ FINDAPET-App/Services/URLContructor/URLParts/Hosts.swift
@@ -10,5 +10,5 @@ import Foundation
 enum Hosts: String {
     case localhost = "127.0.0.1"
     case exchange = "api.exchangerate.host"
-    case base = "47f1-83-234-50-68.ngrok-free.app"
+    case base = "98d5-83-234-50-68.ngrok-free.app"
 }
