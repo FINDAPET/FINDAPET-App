acb9d7f1026795f1e5ee5584d462c20fa51c9a5a
WhyNotWhyYes <artemiy.zuzin@gmail.com>
2023-07-03 22:33:53 +0300

    pre-release 1.1

--- FINDAPET-App/Services/URLContructor/URLParts/Hosts.swift
+++ FINDAPET-App/Services/URLContructor/URLParts/Hosts.swift
@@ -10,6 +10,6 @@ import Foundation
 enum Hosts: String {
     case localhost = "127.0.0.1"
     case exchange = "api.exchangerate.host"
-    case base = "98d5-83-234-50-68.ngrok-free.app"
+    case base = "5c17-83-234-50-68.ngrok-free.app"
     case blueSeaCattery = "bluesea-cattery.ru"
 }
